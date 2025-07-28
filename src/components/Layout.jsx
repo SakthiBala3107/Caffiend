38465c3a3f644385e10fe5ede06596255d229a76
Azula <Azula@gmail.com>
2025-07-28 13:15:22 +0530

    Completion of Header, and Footer

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,8 +1,33 @@
 import React from "react";
 
 const Layout = ({ children }) => {
-  const header = <header></header>;
-  const footer = <footer></footer>;
+  const header = (
+    <header>
+      <div>
+        <h1 className="text-gradient">CAFFIEND</h1>
+        <p>For Coffee Insatiates</p>
+      </div>
+      <button>
+        <p>Sign up for free</p>
+        <i className="fa-solid fa-mug-hot"></i>
+      </button>
+    </header>
+  );
+  const footer = (
+    <footer>
+      <p>
+        <span className="text-gradient">Caffiend </span>was made by{" "}
+        <a target="_blank" href="https://github.com/SakthiBala3107/">
+          Red
+        </a>{" "}
+        using the{" "}
+        <a target="_blank" href="https://www.fantacss.smoljames.com/">
+          Fanta css
+        </a>{" "}
+        design Library
+      </p>
+    </footer>
+  );
 
   return (
     <>
